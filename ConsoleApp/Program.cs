daed8c3e74ac7e00e9d32480d525c846a69d807f
nishant <nishant.singh87@gmail.com>
2021-11-20 18:37:07 +0530

    use a string set command.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -19,6 +19,7 @@ namespace ConsoleApp
       var pingResult = await db.PingAsync();
       Console.WriteLine($"Redis latency : {pingResult.TotalMilliseconds}ms");
       SetStringIfNotAlreadyPresent(db);
+      SetStringIfNotAlreadyPresentWithTTL(db);
     }
 
     public static void SetStringIfNotAlreadyPresent(IDatabase db)
@@ -36,6 +37,28 @@ namespace ConsoleApp
       db.StringSetAsync("my-key", "second value", null, When.NotExists);
       var result2 = db.StringGet("my-key");
       Console.WriteLine($"Value after second set: {result2}");
+    }
+    public static void SetStringIfNotAlreadyPresentWithTTL(IDatabase db)
+    {
+      // Delete key if it exists
+      db.KeyDelete("my-key");
+      
+      // First time should set value
+      db.StringSetAsync("my-key", "first value", TimeSpan.FromMilliseconds(100), When.NotExists);
+      
+      var result = db.StringGet("my-key");
+      Console.WriteLine($"Value after first set: {result}");
+      
+      // Second time should not overwrite first value
+      db.StringSetAsync("my-key", "second value", null, When.NotExists);
+      Console.WriteLine($"Value after second set: {db.StringGet("my-key")}");
+      
+      Task.Delay(TimeSpan.FromMilliseconds(100)).GetAwaiter().GetResult();
+      Console.WriteLine($"Value after TTL: {db.StringGet("my-key")}");
+      
+      // Third time should succeed as value is expired now
+      db.StringSetAsync("my-key", "third value", null, When.NotExists);
+      Console.WriteLine($"Value after third set: {db.StringGet("my-key")}");
     }
   }
 }
--- README.md
+++ README.md
@@ -215,6 +215,40 @@ Following two seem to be most used and best supported :
   > Value after first set: first value
   > Value after second set: first value
 
+- Now lets modify above command to add a TTL 
+
+  ```csharp
+  public static void SetStringIfNotAlreadyPresentWithTTL(IDatabase db)
+  {
+    // Delete key if it exists
+    db.KeyDelete("my-key");
+  
+    // First time should set value
+    db.StringSetAsync("my-key", "first value", TimeSpan.FromMilliseconds(100), When.NotExists);
+  
+    var result = db.StringGet("my-key");
+    Console.WriteLine($"Value after first set: {result}");
+  
+    // Second time should not overwrite first value
+    db.StringSetAsync("my-key", "second value", null, When.NotExists);
+    Console.WriteLine($"Value after second set: {db.StringGet("my-key")}");
+  
+    Task.Delay(TimeSpan.FromMilliseconds(100)).GetAwaiter().GetResult();
+    Console.WriteLine($"Value after TTL: {db.StringGet("my-key")}");
+  
+    // Third time should succeed as value is expired now
+    db.StringSetAsync("my-key", "third value", null, When.NotExists);
+    Console.WriteLine($"Value after third set: {db.StringGet("my-key")}");
+  }
+  ```
+
+  On running the program, you should see folloing output : 
+
+  > Value after first set: first value
+  > Value after second set: first value
+  > Value after TTL: 
+  > Value after third set: third value
+
   
 
 ### References: 
