8cab9219b02e8ebb8bf323ce56cad8a9ea81e5ec
nishant <nishant.singh87@gmail.com>
2021-11-28 15:52:40 +0530

    Set unions

--- ConsoleApp/RedisSets.cs
+++ ConsoleApp/RedisSets.cs
@@ -40,9 +40,11 @@ namespace ConsoleApp
             var setOneKey   = new RedisKey("key-of-my-set-one");
             var setTwoKey   = new RedisKey("key-of-my-set-two");
             var setThreeKey = new RedisKey("key-of-my-set-three");
+            var onePlusTwoKey = new RedisKey("set-one-plus-two");
+            var oneUnionTwo = new RedisKey("set-one-union-two");
 
             // Delete if it exists
-            await db.KeyDeleteAsync(new []{ setOneKey, setTwoKey, setThreeKey });
+            await db.KeyDeleteAsync(new []{ setOneKey, setTwoKey, setThreeKey, onePlusTwoKey, oneUnionTwo });
 
             var setOneItems = new[] {"one", "one-two", "one-two-three"};
             var setTwoItems = new[] {"two", "one-two", "one-two-three"};
@@ -65,6 +67,21 @@ namespace ConsoleApp
             // SMEMBERS: Get all values in a set
             var values = await db.SetMembersAsync(setOneKey);
             Console.WriteLine($"items in one : {string.Join(", ", values.Select(v => v.ToString()))}");
+            
+            // SPOP : Remove and get a random member from set
+            var popped = await db.SetPopAsync(setOneKey);
+            Console.WriteLine($"Popped from one : {popped}");
+            
+            // Subtract two from one and store result in new SORTED SET
+            await db.SortedSetCombineAndStoreAsync(SetOperation.Union, onePlusTwoKey, setOneKey, setTwoKey);
+            var result = await db.SortedSetRangeByValueAsync(onePlusTwoKey);
+            Console.WriteLine($"one union two : {string.Join(", ", result.Select(v => v.ToString()))}");
+            
+            await db.SortedSetCombineAndStoreAsync(SetOperation.Intersect, oneUnionTwo, setOneKey, setTwoKey);
+            // var result = await db.SortedSetRangeByValueAsync("set-one-plus-two");
+            // Console.WriteLine($"items in set-one-plus-two : {string.Join(", ", result.Select(v => v.ToString()))}");
+
+            
         }
     }
 }
